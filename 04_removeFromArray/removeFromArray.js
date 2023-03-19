8796e4f38fc8ea4e40abb85a515879abe2343c2f
LourdenB15 <lourdenbaydal15@gmail.com>
2023-03-19 15:33:29 +0800

    Translate it to a function that accepts unlimited parameters

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,22 @@
-const removeFromArray = function (a, b) {
+// const removeFromArray = function (a, b) {
+//     const newArr = [];
+//     for (let i = 0; i < a.length; i++) {
+//         if (a[i] === b) {
+//             continue;
+//         } else {
+//             newArr.push(a[i]);
+//         }
+//     }
+//     return newArr;
+// };
+
+const removeFromArray = function () {
     const newArr = [];
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] === b) {
+    for (let i = 0; i < arguments[0].length; i++) {
+        if (arguments[0][i] === arguments[1]) {
             continue;
         } else {
-            newArr.push(a[i]);
+            newArr.push(arguments[0][i]);
         }
     }
     return newArr;
