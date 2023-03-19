36fd4c621b6a8eeda341667428afc2866d878124
LourdenB15 <lourdenbaydal15@gmail.com>
2023-03-19 13:43:02 +0800

    Pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (a, b) {
+    const newArr = [];
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] === b) {
+            continue;
+        } else {
+            newArr.push(a[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
